330ad83be4df2b3c778a71a0be272518c924413c
kremi151 <kremi151dev@gmail.com>
2017-04-30 17:25:26 +0200

    Make biomes benefit of id-less registering

--- src/main/java/lu/kremi151/minamod/MinaBiomes.java
+++ src/main/java/lu/kremi151/minamod/MinaBiomes.java
@@ -2,34 +2,29 @@ package lu.kremi151.minamod;
 
 import lu.kremi151.minamod.biome.BiomeRural;
 import lu.kremi151.minamod.biome.BiomeVeld;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.biome.Biome;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class MinaBiomes {
 
 	// TODO: Biomes
 	public final static Biome VELD;
 	public final static Biome RURAL;
-	// public static Biome biomeKremiForest;
-	// public static Biome biomeCherryForest;
 
 	private static boolean init = false;
 
 	static {
 		// TODO: Biomes
-		VELD = new BiomeVeld();
-		RURAL = new BiomeRural();
-		// biomeKremiForest = new BiomeGenKremiForest(204).setBiomeName("Kremi
-		// Forest");
-		// biomeCherryForest = new
-		// BiomeGenCherryForest(205).setBiomeName("Cherry Forest");
+		VELD = new BiomeVeld().setRegistryName(new ResourceLocation(MinaMod.MODID, "veld"));
+		RURAL = new BiomeRural().setRegistryName(new ResourceLocation(MinaMod.MODID, "rural"));
 	}
 
 	static void init() {
 		if (init)throw new RuntimeException("Duplicate call of function");
 
-		int start_id = 208;
-		Biome.registerBiome(start_id, "Veld", VELD);
-		Biome.registerBiome(start_id + 1, "Rural", RURAL);
+		GameRegistry.register(VELD);
+		GameRegistry.register(RURAL);
 
 		init = true;
 	}
