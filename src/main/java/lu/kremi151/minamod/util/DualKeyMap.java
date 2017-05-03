be30483ca4b1a5579e231e7ce9b374b076572191
kremi151 <kremi151dev@gmail.com>
2017-05-03 18:58:39 +0200

    Rewrite of the currently unused DualKeyMap

--- src/main/java/lu/kremi151/minamod/util/DualKeyMap.java
+++ src/main/java/lu/kremi151/minamod/util/DualKeyMap.java
@@ -5,17 +5,19 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import lu.kremi151.minamod.interfaces.ICallback;
 
-public class DualKeyMap<T, U, V> implements Map<DualKeyMap.Key<T, U>, V>{
+public class DualKeyMap<T, U, V> implements Map<Pair<T, U>, V>{
 
-	private final Map<Key<T, U>, V> internalMap;
+	private final Map<Pair<T, U>, V> internalMap;
 
 	public DualKeyMap(){
-		this.internalMap = new HashMap<Key<T, U>, V>();
+		this.internalMap = new HashMap<Pair<T, U>, V>();
 	}
 	
-	public DualKeyMap(ICallback<Map<Key<T, U>, V>> constructor){
+	public DualKeyMap(ICallback<Map<Pair<T, U>, V>> constructor){
 		this.internalMap = constructor.callback();
 	}
 	
@@ -31,7 +33,7 @@ public class DualKeyMap<T, U, V> implements Map<DualKeyMap.Key<T, U>, V>{
 	}
 	
 	public boolean containsKey(T key1, U key2) {
-		return internalMap.containsKey(new Key<T, U>(key1, key2));
+		return internalMap.containsKey(Pair.of(key1, key2));
 	}
 
 	@Override
@@ -40,7 +42,7 @@ public class DualKeyMap<T, U, V> implements Map<DualKeyMap.Key<T, U>, V>{
 	}
 
 	@Override
-	public Set<java.util.Map.Entry<Key<T, U>, V>> entrySet() {
+	public Set<java.util.Map.Entry<Pair<T, U>, V>> entrySet() {
 		return internalMap.entrySet();
 	}
 
@@ -51,7 +53,7 @@ public class DualKeyMap<T, U, V> implements Map<DualKeyMap.Key<T, U>, V>{
 	}
 	
 	public V get(T key1, U key2){
-		return internalMap.get(new Key<T, U>(key1, key2));
+		return internalMap.get(Pair.of(key1, key2));
 	}
 
 	@Override
@@ -60,21 +62,21 @@ public class DualKeyMap<T, U, V> implements Map<DualKeyMap.Key<T, U>, V>{
 	}
 
 	@Override
-	public Set<Key<T, U>> keySet() {
+	public Set<Pair<T, U>> keySet() {
 		return internalMap.keySet();
 	}
 
 	@Override
-	public V put(Key<T, U> key, V value) {
+	public V put(Pair<T, U> key, V value) {
 		return internalMap.put(key, value);
 	}
 	
 	public V put(T key1, U key2, V value){
-		return internalMap.put(new Key<T, U>(key1, key2), value);
+		return internalMap.put(Pair.of(key1, key2), value);
 	}
 
 	@Override
-	public void putAll(Map<? extends Key<T, U>, ? extends V> m) {
+	public void putAll(Map<? extends Pair<T, U>, ? extends V> m) {
 		internalMap.putAll(m);
 	}
 
@@ -91,7 +93,7 @@ public class DualKeyMap<T, U, V> implements Map<DualKeyMap.Key<T, U>, V>{
 	}
 	
 	public V removeKey(T key1, U key2) {
-		return internalMap.remove(new Key<T, U>(key1, key2));
+		return internalMap.remove(Pair.of(key1, key2));
 	}
 
 	@Override
@@ -102,53 +104,5 @@ public class DualKeyMap<T, U, V> implements Map<DualKeyMap.Key<T, U>, V>{
 	@Override
 	public Collection<V> values() {
 		return internalMap.values();
-	}
-	
-	public static class UnorderedKey<T> extends Key<T, T>{
-
-		public UnorderedKey(T key1, T key2) {
-			super(key1, key2);
-		}
-		
-		@Override
-		public boolean equals(Object obj){
-			if(obj instanceof Key){
-				Key other = (Key)obj;
-				return (this.key1.equals(other.key1) && this.key2.equals(other.key2)) || (this.key1.equals(other.key2) && this.key2.equals(other.key1));
-			}else{
-				return false;
-			}
-		}
-		
-		@Override
-		public int hashCode(){
-			return key1.hashCode() ^ key2.hashCode();
-		}
-	}
-	
-	public static class Key<T, U>{
-		
-		protected final T key1;
-		protected final U key2;
-		
-		public Key(T key1, U key2){
-			this.key1 = key1;
-			this.key2 = key2;
-		}
-		
-		@Override
-		public boolean equals(Object obj){
-			if(obj instanceof Key){
-				Key other = (Key)obj;
-				return this.key1.equals(other.key1) && this.key2.equals(other.key2);
-			}else{
-				return false;
-			}
-		}
-		
-		@Override
-		public int hashCode(){
-			return (key1.hashCode() * 31) + key2.hashCode();
-		}
 	}
 }
