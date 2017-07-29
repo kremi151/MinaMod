ce64af529b6e9fa805ce08ce71e9b21c42116245
kremi151 <kremi151dev@gmail.com>
2017-07-29 13:08:04 +0200

    Altered the way pure coins are given to a player
    (Does this fix a bug with Sponge?)

--- src/main/java/lu/kremi151/minamod/capabilities/coinhandler/EntityCoinHandler.java
+++ src/main/java/lu/kremi151/minamod/capabilities/coinhandler/EntityCoinHandler.java
@@ -1,15 +1,17 @@
 package lu.kremi151.minamod.capabilities.coinhandler;
 
 import lu.kremi151.minamod.MinaItems;
+import lu.kremi151.minamod.util.MinaUtils;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemHandlerHelper;
 
 public class EntityCoinHandler extends BaseInventoryCoinHandler{
 	
-	private Entity entity;
+	private final Entity entity;
 
 	public EntityCoinHandler(Entity entity) {
 		super(entity);
@@ -18,10 +20,22 @@ public class EntityCoinHandler extends BaseInventoryCoinHandler{
 
 	@Override
 	protected void giveCoins(int amount) {
+		if(entity.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null)) {
+			IItemHandler inv = (IItemHandler) entity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+			while(amount > 0){
+				final int originAmount = Math.min(MinaItems.GOLDEN_COIN.getItemStackLimit(), amount);
+				ItemStack stack = ItemHandlerHelper.insertItem(inv, new ItemStack(MinaItems.GOLDEN_COIN, originAmount), false);
+				amount -= originAmount;
+				amount += stack.getCount();
+				if(!stack.isEmpty()) {
+					break;
+				}
+			}
+		}
 		while(amount > 0){
-			ItemStack stack = new ItemStack(MinaItems.GOLDEN_COIN, amount % 65);
-			entity.entityDropItem(stack, 0.3f);
-			amount -= 64;
+			ItemStack stack = new ItemStack(MinaItems.GOLDEN_COIN, Math.min(MinaItems.GOLDEN_COIN.getItemStackLimit(), amount));
+			MinaUtils.dropItem(stack, entity.world, entity.posX, entity.posY + 0.5, entity.posZ).setDefaultPickupDelay();
+			amount -= stack.getCount();
 		}
 	}
 
