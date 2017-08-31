5a7a0c862d483ab596b612bac7c3a5c7b853232c
kremi151 <kremi151dev@gmail.com>
2017-08-31 15:09:00 +0200

    Sketch item will now be synced properly

--- src/main/java/lu/kremi151/minamod/item/ItemSketch.java
+++ src/main/java/lu/kremi151/minamod/item/ItemSketch.java
@@ -3,6 +3,7 @@ package lu.kremi151.minamod.item;
 import javax.annotation.Nullable;
 
 import lu.kremi151.minamod.capabilities.sketch.ISketch;
+import lu.kremi151.minamod.interfaces.ISyncCapabilitiesToClient;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
@@ -15,31 +16,54 @@ import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.util.INBTSerializable;
 
-public class ItemSketch extends Item{
+public class ItemSketch extends Item implements ISyncCapabilitiesToClient{
 
 	public ItemSketch() {
 		this.setMaxStackSize(1);
 	}
-	
-    /*@Override
-	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand)
-    {
-    	ISketch cap = player.getHeldItem(hand).getCapability(ISketch.CAPABILITY, null);
-    	for(int y = 0 ; y < 3 ; y++) {
-    		for(int x = 0 ; x < 3 ; x++) {
-    			System.out.print(cap.getOrder().get((y * 3) + x).getDisplayName());
-    			System.out.print(" ");
-    		}
-    		System.out.println();
-    	}
-    	return new ActionResult(EnumActionResult.SUCCESS, player.getHeldItem(hand));
-    }*/
 	
 	@Override
 	public final net.minecraftforge.common.capabilities.ICapabilityProvider initCapabilities(ItemStack stack, @Nullable NBTTagCompound nbt)
     {
         return new SketchCapabilityProvider();
     }
+
+	@Override
+	public NBTTagCompound writeSyncableData(ItemStack stack, NBTTagCompound nbt) {
+		return serializeCapability(stack.getCapability(ISketch.CAPABILITY, null), nbt);
+	}
+
+	@Override
+	public void readSyncableData(ItemStack stack, NBTTagCompound nbt) {
+		deserializeCapability(stack.getCapability(ISketch.CAPABILITY, null), nbt);
+	}
+	
+	private static NBTTagCompound serializeCapability(ISketch cap, NBTTagCompound nbt) {
+		NBTTagList order = new NBTTagList();
+		for(int i = 0 ; i < cap.getOrder().size() ; i++) {
+			ItemStack stack = cap.getOrder().get(i);
+			NBTTagCompound inbt = new NBTTagCompound();
+			
+			ResourceLocation resourcelocation = (ResourceLocation)Item.REGISTRY.getNameForObject(stack.getItem());
+			inbt.setString("id", resourcelocation == null ? "minecraft:air" : resourcelocation.toString());
+			inbt.setByte("Count", (byte)1);
+			inbt.setShort("Damage", (short)stack.getItemDamage());
+	        
+	        order.appendTag(inbt);
+		}
+		nbt.setTag("Order", order);
+		return nbt;
+	}
+	
+	private static void deserializeCapability(ISketch cap, NBTTagCompound nbt) {
+		NBTTagList order = nbt.getTagList("Order", 10);
+		NonNullList list = NonNullList.withSize(9, ItemStack.EMPTY);
+		for(int i = 0 ; i < order.tagCount() ; i++) {
+			list.set(i, new ItemStack(order.getCompoundTagAt(i)));
+		}
+		cap.setCachedRecipe(null);
+		cap.setOrder(list);
+	}
 	
 	private static class SketchCapabilityProvider implements ICapabilityProvider, INBTSerializable<NBTTagCompound>{
 		
@@ -47,31 +71,12 @@ public class ItemSketch extends Item{
 
 		@Override
 		public NBTTagCompound serializeNBT() {
-			NBTTagCompound nbt = new NBTTagCompound();
-			NBTTagList order = new NBTTagList();
-			for(int i = 0 ; i < cap.order.size() ; i++) {
-				ItemStack stack = cap.order.get(i);
-				NBTTagCompound inbt = new NBTTagCompound();
-				
-				ResourceLocation resourcelocation = (ResourceLocation)Item.REGISTRY.getNameForObject(stack.getItem());
-				inbt.setString("id", resourcelocation == null ? "minecraft:air" : resourcelocation.toString());
-				inbt.setByte("Count", (byte)stack.getCount());
-				inbt.setShort("Damage", (short)stack.getItemDamage());
-		        
-		        order.appendTag(inbt);
-			}
-			nbt.setTag("Order", order);
-			return nbt;
+			return serializeCapability(cap, new NBTTagCompound());
 		}
 
 		@Override
 		public void deserializeNBT(NBTTagCompound nbt) {
-			NBTTagList order = nbt.getTagList("Order", 10);
-			cap.order = NonNullList.withSize(9, ItemStack.EMPTY);
-			cap.recipe = null;
-			for(int i = 0 ; i < order.tagCount() ; i++) {
-				cap.order.set(i, new ItemStack(order.getCompoundTagAt(i)));
-			}
+			deserializeCapability(cap, nbt);
 		}
 
 		@Override
