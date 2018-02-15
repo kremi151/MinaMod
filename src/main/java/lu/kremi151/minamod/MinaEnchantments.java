8b7b7d076746849332baef1041a4289b33cfad94
kremi151 <kremi151dev@gmail.com>
2018-02-15 19:19:33 +0100

    Introduction of ObjectHolder annotation for potions and enchantments

--- src/main/java/lu/kremi151/minamod/MinaEnchantments.java
+++ src/main/java/lu/kremi151/minamod/MinaEnchantments.java
@@ -7,49 +7,52 @@ import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnumEnchantmentType;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 import net.minecraftforge.registries.IForgeRegistry;
 
+@ObjectHolder(MinaMod.MODID)
 public class MinaEnchantments {
 
 	private static final EntityEquipmentSlot[] MAIN_HAND_ONLY = new EntityEquipmentSlot[] { EntityEquipmentSlot.MAINHAND };
 
-	public static final Enchantment EMERGENCY;
-	public static final Enchantment BEHEADER;
-	public static final Enchantment WOOD_BACKUP;
-	public static final Enchantment FUSION;
-	public static final Enchantment SPAWN_KEEPER;
-	public static final Enchantment FREEZE_PROTECTION;
-	public static final Enchantment HARVEST_RANGE;
+	@ObjectHolder("emergency")
+	public static final Enchantment EMERGENCY = null;
+	
+	@ObjectHolder("beheader")
+	public static final Enchantment BEHEADER = null;
+	
+	@ObjectHolder("wood_backup")
+	public static final Enchantment WOOD_BACKUP = null;
+	
+	@ObjectHolder("fusion")
+	public static final Enchantment FUSION = null;
+	
+	@ObjectHolder("spawn_keeper")
+	public static final Enchantment SPAWN_KEEPER = null;
+	
+	@ObjectHolder("freeze_protection")
+	public static final Enchantment FREEZE_PROTECTION = null;
+	
+	@ObjectHolder("harvest_range")
+	public static final Enchantment HARVEST_RANGE = null;
 
-	static {
-		EMERGENCY = new CustomEnchantment(Enchantment.Rarity.VERY_RARE, EnumEnchantmentType.WEAPON, MAIN_HAND_ONLY)
-				.setName("emergency").setRegistryName(new ResourceLocation(MinaMod.MODID, "emergency"));
-		BEHEADER = new EnchantmentBeheader(Enchantment.Rarity.RARE, EnumEnchantmentType.WEAPON, MAIN_HAND_ONLY)
-				.setName("beheader").setRegistryName(new ResourceLocation(MinaMod.MODID, "beheader"));
-		WOOD_BACKUP = new CustomEnchantment(Enchantment.Rarity.VERY_RARE, EnumEnchantmentType.WEAPON, MAIN_HAND_ONLY)
-				.setName("wood_backup").setRegistryName(new ResourceLocation(MinaMod.MODID, "wood_backup"));
-		FUSION = new CustomEnchantment(Enchantment.Rarity.UNCOMMON, EnumEnchantmentType.WEAPON, MAIN_HAND_ONLY, true)
-				.setName("fusion").setRegistryName(new ResourceLocation(MinaMod.MODID, "fusion"));
-		SPAWN_KEEPER = new CustomEnchantment(Enchantment.Rarity.UNCOMMON, EnumEnchantmentType.DIGGER, MAIN_HAND_ONLY)
-				.setName("spawn_keeper").setRegistryName(new ResourceLocation(MinaMod.MODID, "spawn_keeper"));
-		FREEZE_PROTECTION = new CustomEnchantment(Enchantment.Rarity.RARE, EnumEnchantmentType.ARMOR,
+	static void register(IForgeRegistry<Enchantment> registry) {
+		registry.register(new CustomEnchantment(Enchantment.Rarity.VERY_RARE, EnumEnchantmentType.WEAPON, MAIN_HAND_ONLY)
+				.setName("emergency").setRegistryName(new ResourceLocation(MinaMod.MODID, "emergency")));
+		registry.register(new EnchantmentBeheader(Enchantment.Rarity.RARE, EnumEnchantmentType.WEAPON, MAIN_HAND_ONLY)
+				.setName("beheader").setRegistryName(new ResourceLocation(MinaMod.MODID, "beheader")));
+		registry.register(new CustomEnchantment(Enchantment.Rarity.VERY_RARE, EnumEnchantmentType.WEAPON, MAIN_HAND_ONLY)
+				.setName("wood_backup").setRegistryName(new ResourceLocation(MinaMod.MODID, "wood_backup")));
+		registry.register(new CustomEnchantment(Enchantment.Rarity.UNCOMMON, EnumEnchantmentType.WEAPON, MAIN_HAND_ONLY, true)
+				.setName("fusion").setRegistryName(new ResourceLocation(MinaMod.MODID, "fusion")));
+		registry.register(new CustomEnchantment(Enchantment.Rarity.UNCOMMON, EnumEnchantmentType.DIGGER, MAIN_HAND_ONLY)
+				.setName("spawn_keeper").setRegistryName(new ResourceLocation(MinaMod.MODID, "spawn_keeper")));
+		registry.register(new CustomEnchantment(Enchantment.Rarity.RARE, EnumEnchantmentType.ARMOR,
 				new EntityEquipmentSlot[] { EntityEquipmentSlot.HEAD, EntityEquipmentSlot.CHEST,
 						EntityEquipmentSlot.LEGS, EntityEquipmentSlot.FEET }).setLevelRange(1, 2)
 								.setName("freeze_protection")
-								.setRegistryName(new ResourceLocation(MinaMod.MODID, "freeze_protection"));
-		HARVEST_RANGE = new EnchantmentHarvestRange(Enchantment.Rarity.RARE, EnumEnchantmentType.WEAPON, MAIN_HAND_ONLY)
-				.setLevelRange(1, 2)
-				.setName("harvest_range")
-				.setRegistryName(new ResourceLocation(MinaMod.MODID, "harvest_range"));
-	}
-
-	static void register(IForgeRegistry<Enchantment> registry) {
-		registry.register(EMERGENCY);
-		registry.register(BEHEADER);
-		registry.register(WOOD_BACKUP);
-		registry.register(FUSION);
-		registry.register(SPAWN_KEEPER);
-		registry.register(FREEZE_PROTECTION);
-		registry.register(HARVEST_RANGE);
+								.setRegistryName(new ResourceLocation(MinaMod.MODID, "freeze_protection")));
+		registry.register(new EnchantmentHarvestRange(Enchantment.Rarity.RARE, EnumEnchantmentType.WEAPON, MAIN_HAND_ONLY)
+				.setLevelRange(1, 2).setName("harvest_range").setRegistryName(new ResourceLocation(MinaMod.MODID, "harvest_range")));
 	}
 }
--- src/main/java/lu/kremi151/minamod/MinaPotions.java
+++ src/main/java/lu/kremi151/minamod/MinaPotions.java
@@ -4,25 +4,27 @@ import lu.kremi151.minamod.potion.CustomPotion;
 import lu.kremi151.minamod.potion.PotionDoge;
 import lu.kremi151.minamod.util.MinaUtils;
 import net.minecraft.potion.Potion;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 import net.minecraftforge.registries.IForgeRegistry;
 
+@ObjectHolder(MinaMod.MODID)
 public class MinaPotions {
+	
+	@ObjectHolder("freeze")
+	public static final Potion FREEZE = null;
 
-	public static final Potion FREEZE;
-	public static final Potion SATURATION;
-	public static final Potion DOGE;
+	@ObjectHolder("saturation")
+	public static final Potion SATURATION = null;
 
-	static{
-		FREEZE = new CustomPotion(true, 13713439).setIconIndex(0, 0)
-				.setPotionName("potion.minamod.freeze").setRegistryName(MinaMod.createResourceLocation("freeze"));
-		SATURATION = new CustomPotion(false, 1837285)
-				.setIconIndex(0, 0).setPotionName("potion.minamod.saturation").setRegistryName(MinaMod.createResourceLocation("saturation"));
-		DOGE = new PotionDoge(false, MinaUtils.convertRGBToDecimal(255, 128, 0)).setPotionName("potion.minamod.doge").setRegistryName(MinaMod.createResourceLocation("doge"));
-	}
+	@ObjectHolder("doge")
+	public static final Potion DOGE = null;
 	
 	static void register(IForgeRegistry<Potion> registry){
-		registry.register(FREEZE);
-		registry.register(SATURATION);
-		registry.register(DOGE);
+		registry.register(new CustomPotion(true, 13713439).setIconIndex(0, 0)
+				.setPotionName("potion.minamod.freeze").setRegistryName(MinaMod.createResourceLocation("freeze")));
+		registry.register(new CustomPotion(false, 1837285).setIconIndex(0, 0)
+				.setPotionName("potion.minamod.saturation").setRegistryName(MinaMod.createResourceLocation("saturation")));
+		registry.register(new PotionDoge(false, MinaUtils.convertRGBToDecimal(255, 128, 0))
+				.setPotionName("potion.minamod.doge").setRegistryName(MinaMod.createResourceLocation("doge")));
 	}
 }
